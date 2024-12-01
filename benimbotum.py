0b6f6f3a53cbad85f805e508f82159602d68de2d
Ahmet Refik Şener <156889520+krefikk@users.noreply.github.com>
2024-12-01 14:08:03 +0300

    Create benimbotum.py

---
+++ benimbotum.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def coin(ctx):
+    yazi_tura_liste = ["yazı", "tura"]
+    sonuc = random.choice(yazi_tura_liste)
+    await ctx.send(sonuc)
+
+bot.run(token)
